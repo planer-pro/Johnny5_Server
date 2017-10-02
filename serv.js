365aa7de0cf83177fccba4bc23764e2d7516a734
planer-pro <planer.reg@gmail.com>
2017-10-02 16:59:37 +0300

    add feedback for input messages

--- serv.js
+++ serv.js
@@ -6,10 +6,28 @@ var bodyParser = require('body-parser');
 //J5
 const five = require('johnny-five');
 const board = new five.Board({ port: "COM11" });
+
 var led;
+var lcd;
+var lcdText;
+var counter = 0;
+
 board.on('ready', () => {
     console.log("board connected");
     led = new five.Led(13);
+    led.on();
+
+    lcd = new five.LCD({
+        controller: "PCF8574T",
+        rows: 4,
+        cols: 20
+    });
+
+    //socketIO counter 
+    /*setInterval(function(){
+        counter++;
+        console.log(counter);
+    },1000);*/
 });
 
 app.set('view engine', 'jade');
@@ -17,19 +35,17 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!',ledState:led.isOn });
+    res.render('index', { title: 'Hey', message: 'Hello there!', readyState: led.isOn, returnText: lcdText });
 });
 
 app.post('/', function (req, res) {
-    if (req.body.val == "1")
-        led.on();
-    else
-        led.off();
+    lcdText = req.body.val;
+    lcd.clear();
+    lcd.print(lcdText);
 
-        res.render('index', { title: 'Hey', message: 'Hello there!',ledState:led.isOn });
+    res.render('index', { title: 'Hey', message: 'Hello there!', readyState: led.isOn, returnText: lcdText });
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port http://localhost:3000');
 });
-
